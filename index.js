3e0bdd8a8d19ea4409d561d5c95e1bd2b1ddf26a
0669-DI <0669@dawatulimaan.org>
2026-07-09 16:34:37 +0000

    Created a has_won() funcshin

--- index.js
+++ index.js
@@ -3,5 +3,19 @@ class Player {
         this.name = name;
         this.cards = [];
     }
+
+    has_won() {
+        if (this.cards.length === 0) {
+            return true;
+        }
+        return false;
+    }
+
+
 }
 
+const player1 = new Player("Yahya");
+
+
+console.log(player1)
+console.log(player1.has_won())
